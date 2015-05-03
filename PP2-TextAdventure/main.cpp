5762d31193ede08225fc88925ed2e65cd5eb0ac9
Daniel Caridade <ruidanielcaridade@gmail.com>
2015-05-03 17:50:54 +0100

    Clean up.

--- PP2-TextAdventure/main.cpp
+++ PP2-TextAdventure/main.cpp
@@ -2,24 +2,9 @@
 #include <SFML\Graphics.hpp>
 #include "LinkedList.h"
 
-int main()
-{
-	LinkedList<int> cabron;
-	LinkedList<char*> elMatador;
-	LinkedList<LinkedList<int>> listaDuplamenteLigada;
-
-	cabron.add(1);
-	cabron.add(3);
-	elMatador.add("Hell");
-
-	printf("%i\n", cabron.get(0));
-	printf("%i\n", cabron.get(1));
-	printf("%s\n", elMatador.get(0));
-
+int main() {
 	Game game;
 	game.run();
-
-	getchar();
 
 	return 0;
 }
