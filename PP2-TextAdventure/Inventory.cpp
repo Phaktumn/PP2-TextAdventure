12b48801080e67994444b5893fac214781027ef7
Jorge Paiva <fskjorge@gmail.com>
2015-06-22 03:43:09 +0100

    Inventory a trabalhar! e a ser desenhado !!pronto!!

--- PP2-TextAdventure/Inventory.cpp
+++ PP2-TextAdventure/Inventory.cpp
@@ -71,3 +71,58 @@ void Inventory::equipArmor(Armor* armor, Armor::Type type)
 		break;
 	}
 }
+
+void Inventory::draw(sf::RenderWindow* window, sf::Font &font){
+	if (head != nullptr){
+		drawText(20, 20, "Head: ", font, 24, window);
+		head->draw(window, &font, 100, 20);
+	}
+	else drawText(20, 20, "Head: No Item", font, 24, window);
+
+	if (chest != nullptr){
+		drawText(20, 40, "Chest: ", font, 24, window);
+		chest->draw(window, &font, 100, 40);
+	}
+	else drawText(20, 40, "Chest: No Item", font, 24, window);
+
+	if (hands != nullptr){
+		drawText(20, 60, "Hands: ", font, 24, window);
+		hands->draw(window, &font, 100, 60);
+	}
+	else drawText(20, 60, "Hands: No Item", font, 24, window);
+
+	if (weapon != nullptr){
+		drawText(20, 80, "Weapon: ", font, 24, window);
+		weapon->draw(window, &font, 110, 83);
+	}
+	else drawText(20, 80, "Weapon: No Item", font, 24, window);
+
+	if (legs != nullptr){
+		drawText(20, 100, "Legs: ", font, 24, window);
+		legs->draw(window, &font, 100, 100);
+	}
+	else drawText(20, 100, "Legs: No Item", font, 24, window);
+
+	if (feet != nullptr){
+		drawText(20, 120, "Feet: ", font, 24, window);
+		feet->draw(window, &font, 100, 120);
+	}
+	else drawText(20, 120, "Feet: No Item", font, 24, window);
+}
+
+
+void Inventory::drawText(float x, float y, const std::string& text, sf::Font& font, int size, sf::RenderWindow* window){
+	sf::Text _text;
+	_text.setPosition(x, y);
+	_text.setString(text);
+	_text.setFont(font);
+	_text.setCharacterSize(size);
+
+	window->draw(_text);
+}
+void Inventory::drawText(float x, float y, sfe::RichText text, int size, sf::RenderWindow* window){
+	text.setPosition(x, y);
+	text.setCharacterSize(size);
+
+	window->draw(text);
+}
--- PP2-TextAdventure/Inventory.h
+++ PP2-TextAdventure/Inventory.h
@@ -6,6 +6,7 @@
 #include "Weapon.h"
 #include "Armor.h" 
 #include "Actor.h"
+#include "RichText.hpp"
 
 class Player;
 
@@ -18,7 +19,10 @@ public:
 	void equipWeapon(Weapon* weapon);
 	void equipArmor(Armor* armor, Item::Type type);
 
-	void draw(sf::RenderWindow* window);
+	void draw(sf::RenderWindow* window, sf::Font &font);
+
+	void drawText(float x, float y, const std::string& text, sf::Font& font, int size, sf::RenderWindow* window);
+	void drawText(float x, float y, sfe::RichText text, int size, sf::RenderWindow* window);
 
 private:
 	LinkedList<Item> bag;
